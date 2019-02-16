b5f9a9b70607b428534649f93c4ba1de8b079840
anto004 <anto_004@live.com>
2019-02-15 18:30:35 -0800

    Add styling to cards in CardListView

--- components/CardListView.js
+++ components/CardListView.js
@@ -1,21 +1,21 @@
 import React, {Component} from "react";
-import {View, Text}from "react-native";
+import {ScrollView, StyleSheet, View, Text}from "react-native";
 import {connect}from "react-redux";
 import {CARD} from "../reducers";
+import {black, lightGray, white} from "../utils/colors";
 
 class CardListView extends Component{
   render(){
     const {cards} = this.props;
     return(
-        <View>
-          <Text>Cards:</Text>
+        <ScrollView contentContainerStyle={styles.outerContainer}>
           {cards.map((card) => (
-              <View key={card.id}>
-                <Text>{JSON.stringify(card.question)}</Text>
-                <Text>{JSON.stringify(card.answer)}</Text>
+              <View key={card.id} style={styles.card}>
+                <Text style={styles.QAText}>Q: {card.question}</Text>
+                <Text style={styles.QAText}>A: {card.answer}</Text>
               </View>
           ))}
-        </View>
+        </ScrollView>
     )
   }
 }
@@ -31,5 +31,30 @@ function dispatchStateToProps(dispatch){
   return {
   }
 }
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    flexGrow: 1,
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  card: {
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    backgroundColor: lightGray,
+    borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingTop: 6,
+    paddingRight: 10,
+    paddingBottom: 6,
+  },
+  QAText: {
+    fontFamily: "Arial",
+    fontWeight: "bold",
+  }
+});
 
 export default connect(mapStateToProps, dispatchStateToProps) (CardListView);
